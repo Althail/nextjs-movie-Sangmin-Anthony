632641452b3d25b25214b106f0aabd57c9d700d0
Sangmin SHIM <tkdals4723@gmail.com>
2024-03-21 10:24:57 +0100

    Comments done

--- movie-app/pages/api/movie/[idMovie]/comments.js
+++ movie-app/pages/api/movie/[idMovie]/comments.js
@@ -1,6 +1,41 @@
 import clientPromise from "../../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
+/***
+ * @swagger
+ * /api/movie/{idMovie}/comments:
+ *   get:
+ *     tags:
+ *      - Comments
+ *     description: Returns comments for specific movie
+ *     parameters:
+ *      - name: idMovie
+ *        in: path
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *     responses:
+ *       200:
+ *         description: Comments List
+ *   post:
+ *     tags:
+ *      - Comments
+ *     description: Create comment for specific movie
+ *     parameters:
+ *       - name: idMovie
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: string
+ *     requestBody:
+ *       description: Create a comment for a movie
+ *       content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ */
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("sample_mflix");
